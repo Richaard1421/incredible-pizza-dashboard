89d865d28c8aebd8f27772fa72765773337b28d6
Richaard1421 <ricardosilvaolvera@gmail.com>
2026-05-21 16:50:46 -0600

    Fix PORT for Railway

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const https = require('https');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const API_HOST = 'dev.sacoacard.com';
 const API_PORT = 33033;
 
@@ -63,8 +63,6 @@ app.post('/api/dcsReports/gamesActivityByGroup', proxyPost);
 app.post('/api/dcsReports/sales', proxyPost);
 app.post('/api/dcsReports/salesDetails', proxyPost);
 
-// Proxy para Google Sheets
-const http = require('http');
 app.get('/sheets/:sheetId', function(req, res) {
   var sheetId = req.params.sheetId;
   var sheetName = req.query.sheet || '';
